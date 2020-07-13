a3163caedd6d6efb532788d1f5f210de6616b02c
nafisehnikeghbal <nafise.nikeghbal32@gmail.com>
2020-07-13 18:56:09 +0430

    Question 4 and MainClass

--- src/com/seleniumProject/Main.java
+++ src/com/seleniumProject/Main.java
@@ -8,7 +8,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,54 +22,70 @@ import org.openqa.selenium.remote.RemoteWebElement;
 import java.util.List;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.WeakHashMap;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        SeleniumTest st = new SeleniumTest();
-        //1
-        st.tableHeaders();
-        //2
-        st.testHtmlHaveLangAttribute();
-        //3
-        st.pageHasTitle();
-        //4
-        st.setAndFindDeprecatedAttributes();
-        //5
-        st.imageHeightWidth();
-        //6
-        st.tabeDescription();
-        //7
-        st.checkLabel();
-        //8
-        st.sameTargetAndText();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("If You Want WCAG Test Enter:1 \n If You Want Semantic Relatedness Enter:2 \n Exit With 0");
+            System.out.println("###########################################################################");
+            int choose = scanner.nextInt();
+            if (choose == 1) {
+
+                SeleniumTest st = new SeleniumTest();
+                //1
+                st.tableHeaders();
+                //2
+                st.testHtmlHaveLangAttribute();
+                //3
+                st.pageHasTitle();
+                //4
+                st.setAndFindDeprecatedAttributes();
+                //5
+                st.imageHeightWidth();
+                //6
+                st.tabelDescription();
+                //7
+                st.checkLabel();
+                //8
+                st.sameTargetAndText();
+                System.out.println("###########################################################################");
+                System.out.println("If You Want WCAG Test Enter:1\nIf You Want Semantic Relatedness Enter:2\nExit With 0");
+                choose = scanner.nextInt();
+            }
 
 
-       /*
-        try(BufferedReader in = new BufferedReader(new FileReader("G:\\lesson\\Software Test\\Project\\SeleniumProject\\input.txt"))) {
-            String str;
-            SemanticRelatedness sr = new SemanticRelatedness();
-            while ((str = in.readLine()) != null) {
-                System.out.println(str);
-                String[] tokens = str.split(",");
-                String phrase1 = tokens[0];
-                String phrase2 = tokens[1];
-                if(sr.foundPage(phrase1,phrase2)) sr.searchString(phrase1,phrase2);
-                System.out.println("#############################################");
+            if (choose == 2) {
+                try (BufferedReader in = new BufferedReader(new FileReader("G:\\lesson\\Software Test\\Project\\SeleniumProject\\input.txt"))) {
+                    String str;
+                    SemanticRelatedness sr = new SemanticRelatedness();
+                    while ((str = in.readLine()) != null) {
+                        System.out.println(str);
+                        String[] tokens = str.split(",");
+                        String phrase1 = tokens[0];
+                        String phrase2 = tokens[1];
+                        if (sr.foundPage(phrase1, phrase2)) sr.searchString(phrase1, phrase2);
+                        System.out.println("**************************************");
+
+                    }
+                } catch (IOException e) {
+                    System.out.println("File Read Error");
+                }
+                System.out.println("###########################################################################");
+                System.out.println("If You Want WCAG Test Enter:1 \n If You Want Semantic Relatedness Enter:2 \n Exit With 0");
+                choose = scanner.nextInt();
+
 
             }
-        }
-        catch (IOException e) {
-            System.out.println("File Read Error");
-        }
 
-*/
-
+            if(choose==0){
+                break;
+            }
+        }
     }
 
 
--- src/com/seleniumProject/SeleniumTest.java
+++ src/com/seleniumProject/SeleniumTest.java
@@ -31,7 +31,7 @@ public class SeleniumTest {
     }
 
 
-
+    //------------------------------------  2  ---------------------------------------
 
     public void testHtmlHaveLangAttribute() {
         WebElement html = driver.findElement(By.tagName("html"));
@@ -71,7 +71,6 @@ public class SeleniumTest {
         // get all "for" attributes of labales
         List<WebElement> labels = driver.findElements(By.tagName("label"));
         List<String> labelfors = new ArrayList<>();
-        System.out.println(labels.size());
         for (WebElement WE : labels) {
             labelfors.add(WE.getAttribute("for"));
         }
@@ -100,8 +99,9 @@ public class SeleniumTest {
             }
         }
     }
+    //---------------------------------------  6  ------------------------------------
     /////////////////////////////////firt remove table that have figure Tag/////////////////////////////////
-    public void tabeDescription() {
+    public void tabelDescription() {
         int tableCount=0;
         List<WebElement> tabels = driver.findElements(By.tagName("table"));
         List<WebElement> figures = driver.findElements(By.tagName("figure"));
@@ -131,6 +131,8 @@ public class SeleniumTest {
         }
     }
 
+
+    //---------------------------------------  8  ------------------------------------
     public void sameTargetAndText(){
         List<WebElement> links = driver.findElements(By.tagName("a"));
         for(int i=0 ; i<links.size() ; i++){
@@ -183,14 +185,74 @@ public class SeleniumTest {
 
 
     private void checkExceptionCase(){
-        List<WebElement> tags = driver.findElements(By.tagName("img"));
-        for (WebElement tag : tags){
-            String deprecatedAttr = tag.getAttribute("border");
-            //List<WebElement> tags  = driver.findElements(By.xpath("//img[@border]"));
-            System.out.println(deprecatedAttr);
+        List <WebElement> tags  = driver.findElements(By.tagName("script"));
+        for(WebElement t: tags){
+            String language = t.getAttribute("language");
+            if(language!=null && !language.isEmpty()) {
+                if (!language.toLowerCase().equals("javascript")) {
+                    System.out.println("\"language\" is a deprecated attribute if the value:\"" + language + "\"");
+                }
             }
 
         }
+
+        tags.clear();
+        tags  = driver.findElements(By.tagName("img"));
+        for(WebElement t: tags){
+            String border = t.getAttribute("border");
+            //String name = t.getAttribute("name");
+            if(border!=null && !border.isEmpty()) {
+                if (!border.toLowerCase().equals("0")) {
+                    System.out.println("\"border\" is a deprecated attribute if the value:\"" + border + "\"");
+                }
+            }
+
+        }
+
+        tags.clear();
+        tags  = driver.findElements(By.tagName("a"));
+        for(WebElement t: tags){
+            String id = t.getAttribute("id");
+            String name = t.getAttribute("name");
+            //String name = t.getAttribute("name");
+            if(name!=null && !name.isEmpty()) {
+                if(id!=null && !id.isEmpty()) {
+                    if (!name.equals(id)) {
+                        System.out.println("\"name\" is a deprecated attribute if the value:\"" + name + "\"");
+                    }
+                }
+            }
+
+        }
+        /*
+        for(WebElement t: tags){
+            if(!t.toString().equals("0")){
+                System.out.println("\"border\" is a deprecated attribute if the value is: \""+t.toString()+"\"");
+            }
+        }
+        tags.clear();
+        tags  = driver.findElements(By.xpath("//script[@language]"));
+        for(WebElement t: tags){
+            if(!t.toString().toLowerCase().equals("javascript")){
+                System.out.println("\"language\" is a deprecated attribute if the value is:\""+t.toString().toLowerCase()+"\"");
+            }
+
+        }
+
+        tags.clear();
+        tags  = driver.findElements(By.xpath("//a"));
+        for(WebElement t: tags){
+            String id = t.getAttribute("id");
+            String name = t.getAttribute("name");
+            if(!id.equals(name)){
+                System.out.println("\"name\" is a deprecated attribute if the value:\""+name+"\"");
+            }
+
+        }
+
+*/
+
+    }
 
 
 
@@ -204,10 +266,20 @@ public class SeleniumTest {
 
         }
     }
+    private void checkDeprecatedAttributesImage(List<String> attList) {
+        for(String s : attList){
+            String str = "//img[@"+s+"]";
+            int num = driver.findElements(By.xpath(str)).size();
+            if(num!=0){
+                System.out.println("\""+s+"\""+" is a deprecated Attributes");
+            }
 
+        }
+    }
 
 
     public void setAndFindDeprecatedAttributes(){
+        checkExceptionCase();
         List<String> attList = new ArrayList<>();
         List<String> tagsList = new ArrayList<>();
         attList.addAll(Arrays.asList("alink","background","bgcolor","link","marginbottom","marginheight","marginleft","marginright",
@@ -247,8 +319,12 @@ public class SeleniumTest {
         checkDeprecatedAttributes(attList,"a");
         attList.clear();
 
+        attList.addAll(Arrays.asList("align","name","datafld","datasrc","hspace","lowsrc"));
+        checkDeprecatedAttributesImage(attList);
+        attList.clear();
+
         ////////////////single attribute//////////////////////////////////////////////
-        tagsList.addAll(Arrays.asList(	"caption","col","div","embed","h1","h2","h3","h4","h5","h6","hr","iframe","img","input","legend","p","tbody","thead","tfoot","td","th","tr"));
+        tagsList.addAll(Arrays.asList(	"caption","col","div","embed","h1","h2","h3","h4","h5","h6","hr","iframe","input","legend","p","tbody","thead","tfoot","td","th","tr"));
         checkDeprecatedAttributes2("align",tagsList);
         tagsList.clear();
 
@@ -257,7 +333,7 @@ public class SeleniumTest {
         checkDeprecatedAttributes2("background",tagsList);
         tagsList.clear();
 
-        tagsList.addAll(Arrays.asList("embed","img","optiont"));
+        tagsList.addAll(Arrays.asList("embed","optiont"));
         checkDeprecatedAttributes2("name",tagsList);
         tagsList.clear();
 
@@ -281,7 +357,7 @@ public class SeleniumTest {
         checkDeprecatedAttributes2("axis",tagsList);
         tagsList.clear();
 
-        tagsList.addAll(Arrays.asList("a","applet","button","div","fieldset","frame","iframe","img","input","label","legend","marquee","param","select",
+        tagsList.addAll(Arrays.asList("a","applet","button","div","fieldset","frame","iframe","input","label","legend","marquee","param","select",
                 "span","textare"));
         checkDeprecatedAttributes2("datafld",tagsList);
         tagsList.clear();
@@ -291,11 +367,11 @@ public class SeleniumTest {
         tagsList.clear();
 
 
-        tagsList.addAll(Arrays.asList("a","applet","button","div","frame","iframe","img","input","label","legend","marquee","option","select","span","textarea"));
+        tagsList.addAll(Arrays.asList("a","applet","button","div","frame","iframe","input","label","legend","marquee","option","select","span","textarea"));
         checkDeprecatedAttributes2("datasrc",tagsList);
         tagsList.clear();
 
-        tagsList.addAll(Arrays.asList("embed","iframe","img","input"));
+        tagsList.addAll(Arrays.asList("embed","iframe","input"));
         checkDeprecatedAttributes2("hspace",tagsList);
         tagsList.clear();
 
@@ -336,7 +412,6 @@ public class SeleniumTest {
         ///////////////////////single tag single attribute///////////
         checkDeprecatedAttributes3("accept","form");
         checkDeprecatedAttributes3("clear","br");
-        checkDeprecatedAttributes3("lowsrc","img");
         checkDeprecatedAttributes3("nohref","area");
         checkDeprecatedAttributes3("profile","head");
         checkDeprecatedAttributes3("scheme","meta");
@@ -345,6 +420,8 @@ public class SeleniumTest {
         checkDeprecatedAttributes3("valuetype","param");
         checkDeprecatedAttributes3("version","html");
     }
+
+
 
 }
 
